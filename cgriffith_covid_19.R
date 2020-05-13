5beb5ac224f3afa02c0413454fc600bec523737c
cgriffithjr <2cgrjr@gmail.com>
2020-05-13 10:59:18 -0400

    Add filename to 'dailies' dataset made by combing comma-delimited files

--- cgriffith_covid_19.R
+++ cgriffith_covid_19.R
@@ -13,10 +13,14 @@ path1 <- "C:/Users/Mark/Documents/COVID-19/csse_covid_19_data/csse_covid_19_time
 path2 <- "C:/Users/Mark/Documents/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports_us/"
 list.files(path2)
 
-dailies <- list.files(path2, pattern = "csv$") %>%
-            paste0(path2, .) %>%
-            map_df(read_csv, .id = "source")
+dailies <- list.files(path2, pattern = "csv$", full.names = T) %>%
+  set_names(nm = (basename(.) %>% tools::file_path_sans_ext())) %>%
+  map_df(read_csv, .id = "filename") %>%
+  mutate(date_ = mdy(filename))
 
+ 
+names(dailies)  
+unique(dailies$date_)
 glimpse(dailies)
   
 
@@ -26,59 +30,66 @@ glimpse(case_ts)
 dim(case_ts)
 
 
-daily_ex <- read_csv(paste0(path2, "04-12-2020.csv"))
-glimpse(daily_ex)
+# daily_ex <- read_csv(paste0(path2, "04-12-2020.csv"))
+# glimpse(daily_ex)
+# unique(daily_ex$Last_Update)
+# daily_ex$date_ <- as.Date(daily_ex$Last_Update)
 
-deaths_ts <- read_csv(paste0(path1, "time_series_covid19_deaths_US.csv"))
-glimpse(deaths_ts)
+# deaths_ts <- read_csv(paste0(path1, "time_series_covid19_deaths_US.csv"))
+# glimpse(deaths_ts)
 
-cases_greene <- case_ts %>%
-  filter(Province_State == "Ohio" & Admin2 == "Greene")
+# cases_greene <- case_ts %>%
+#   filter(Province_State == "Ohio" & Admin2 == "Greene")
 
-deaths_greene <- deaths_ts %>%
-  filter(Province_State == "Ohio" & Admin2 == "Greene")
+# deaths_greene <- deaths_ts %>%
+#   filter(Province_State == "Ohio" & Admin2 == "Greene")
 
-write_csv(cases_greene, "cases_greene.csv")
-write_csv(deaths_greene, "deaths_greene.csv")
+# write_csv(cases_greene, "cases_greene.csv")
+# write_csv(deaths_greene, "deaths_greene.csv")
 
 # Function to select data by dates and convert format from wide to long
-pivot_func <- function(df, date1 = "1/22/20", date2 = "5/5/20", named = "date_", valued = "cases"){
-  ts_pivot <- df %>% pivot_longer(
-    date1:date2, 
-    names_to = named,
-    values_to = valued
-  )
-  ts_pivot$date_ = mdy(ts_pivot$date_)
-  return(ts_pivot)
-}
+# pivot_func <- function(df, date1 = "1/22/20", date2 = "5/5/20", named = "date_", valued = "cases"){
+#   ts_pivot <- df %>% pivot_longer(
+#     date1:date2, 
+#     names_to = named,
+#     values_to = valued
+#   )
+#   ts_pivot$date_ = mdy(ts_pivot$date_)
+#   return(ts_pivot)
+# }
 
-case_pivot <- pivot_func(case_ts)
-glimpse(case_pivot)
+# case_pivot <- pivot_func(case_ts)
+# glimpse(case_pivot)
 
-deaths_pivot <- pivot_func(deaths_ts, valued = "deaths")
-glimpse(deaths_pivot)
+# deaths_pivot <- pivot_func(deaths_ts, valued = "deaths")
+# glimpse(deaths_pivot)
 
-cases_deaths <- inner_join(case_pivot, deaths_pivot, by = c("UID" = "UID", "iso2" = "iso2", 
-                                                            "iso3" = "iso3", "code3" = "code3", 
-                                                            "FIPS" = "FIPS", "Admin2" = "Admin2",
-                                                            "Country_Region" = "Country_Region",
-                                                            "Province_State" = "Province_State", 
-                                                            "Lat" = "Lat", "Long_" = "Long_" , 
-                                                            "Combined_Key" = "Combined_Key", "date_" = "date_" ))
-glimpse(cases_deaths)
+# cases_deaths <- inner_join(case_pivot, deaths_pivot, by = c("UID" = "UID", "iso2" = "iso2", 
+#                                                             "iso3" = "iso3", "code3" = "code3", 
+#                                                             "FIPS" = "FIPS", "Admin2" = "Admin2",
+#                                                             "Country_Region" = "Country_Region",
+#                                                             "Province_State" = "Province_State", 
+#                                                             "Lat" = "Lat", "Long_" = "Long_" , 
+#                                                             "Combined_Key" = "Combined_Key", "date_" = "date_" ))
+# glimpse(cases_deaths)
 
-oh <- cases_deaths %>%
-  filter(Province_State == "Ohio")
+# oh <- cases_deaths %>%
+#   filter(Province_State == "Ohio")
+# Greene <- oh %>%
+#   filter(Admin2 == "Greene")
+# Greene_april <- Greene %>%
+#   filter(date_ >= mdy("4/1/20") & date_ <= mdy("4/30/20"))
 
-Greene <- oh %>%
-  filter(Admin2 == "Greene")
+# write_csv(Greene_april, "Greene_april_combo.csv")
 
-Greene_april <- Greene %>%
-  filter(date_ >= mdy("4/1/20") & date_ <= mdy("4/30/20"))
-write_csv(Greene_april, "Greene_april_combo.csv")
+oh2 <- dailies %>%
+  arrange(date_) %>%
+  filter(Country_Region == "US" & Province_State  == "Ohio") %>%
+  filter(date_ >= mdy("04-12-2020") & date_ <= mdy("05-05-2020"))
+glimpse(oh2)
 
 
-ts_plt_func <- function(df, xvar1 = date_,yvar1 = cases, yvar2 = deaths, xlab = "Date", titled = "Corvid-19 Cases & Deaths in Greene County, OH", 
+ts_plt_func <- function(df, xvar1 = date_, yvar1 = Active, yvar2 = Deaths, xlab = "Date", titled = "Corvid-19 Cases & Deaths in Greene County, OH", 
                         ylab1 = "Cases", ylab2 = "Deaths", color1 = "blue", color2 = "red"){
   xvar1 <- enquo(xvar1)
   yvar1 <- enquo(yvar1)
@@ -107,8 +118,9 @@ ts_plt_func <- function(df, xvar1 = date_,yvar1 = cases, yvar2 = deaths, xlab = 
   print(plt1)
 } 
 
-ts_plt_func(Greene_april)
-
+ts_plt_func(oh2, titled = "Covid-19 Cases & Deaths in Ohio")
+ts_plt_func(oh2, yvar1 = Testing_Rate, yvar2 = Mortality_Rate, ylab1 = "Testing Rate", ylab2 = "Mortality Rate",
+            titled = "Covid-19 Testing & Mortality Rates in Ohio")
 
 ts_plt_func2 <- function(df, xlab = "Date", ylab1 = "Cases"){
   
